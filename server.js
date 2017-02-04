c79a3daeb615efca3ddb6a854bb7031ad3c9091f
Jason-Pearson <jason-pearson-1-0-7@hotmail.com>
2017-02-03 20:19:10 -0500

    Created demo of Connect Server

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 // our connect server - for git initial commit
+let connect = require('connect');
+ 
+let app = connect();
+const port = 3000;
+
+app.listen(port); //listen for port#/hostname - localhost:3000
+console.log(`Server listening at port: ${port}`); //need ` back quotes on tilde key ~ for passing variables into string
+
+
+// hello route - in proper order via mounting - '/(insert)' first before default '/'
+app.use('/hello', (req, res, next) =>{
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Hello FAM!")
+  next();
+});
+
+//anonymous Function with Arrow Function - indicate this root folder for the server to access - respond to the html page with "Welcome"
+//app.use same as app.get
+app.use('/', (req, res, next) =>{
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Welcome!")
+  next();
+});
